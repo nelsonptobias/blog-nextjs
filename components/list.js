b8e2099d2c95447be7fe4bb378d5b899f8a20597
nelson <nelsonptobias@gmail.com>
2020-11-13 08:47:37 -0300

    [fix] adicionando colunas responsividas (ainda com problemas)

--- components/list.js
+++ components/list.js
@@ -1,33 +1,37 @@
-import { List, Stat, StatNumber, Text, Image, ListItem, StatHelpText, Button, Flex, Box, SimpleGrid } from "@chakra-ui/core"
+import { List, Stat, StatNumber, Text, Image, ListItem, StatHelpText, Button, Flex, Box, StatGroup } from "@chakra-ui/core";
 
 export default function ListaProdutos({ lista, pesquisar }) {
-
-    return (
-        <List>
-            {lista && lista.map(produto =>
-                <ListItem key={produto.id} >
-                    <Text fontSize="md">{produto.nomeProduto}</Text>
-                    <Text fontSize="md" align='center' >Preco com desconto no boleto</Text>
-                    <Flex align="flex-end">
-                        <Image src={produto.img} alt="produto" />
-
-                        <Button colorScheme="blue" onClick={(e) => pesquisar(e, produto.url)}  >Button</Button>
-
-                        <Box display='flex' flexDirection='column' flexWrap='wrap' height='400px' >
-                            {produto.consultas.map((consulta, index) =>
-
-                                <Stat diplay='flex' borderWidth="1px" borderStyle='solid' key={consulta.id} >
-                                    <StatNumber >{consulta.preco}</StatNumber>
-                                    <StatHelpText>{consulta.data}</StatHelpText>
-                                </Stat>
-
-                            )}
-                        </Box>
-                    </Flex>
-                </ListItem>
-            )
-            }
-        </List >
-
-    )
+  return (
+    <List>
+      {lista &&
+        lista.map((produto) => (
+          <ListItem key={produto.id}>
+            <Box display="flex" flexDirection={{ lg: "row", md: "column" }}>
+              <Box display="flex" flex={1} flexDirection="column" alignItems="center" border="solid 1px #000">
+                <Text fontSize="md">{produto.nomeProduto}</Text>
+                <Image src={produto.img} alt="produto" />
+                <Button colorScheme="blue" onClick={(e) => pesquisar(e, produto.url)}>
+                  Button
+                </Button>
+              </Box>
+              <Flex flex={2} flexDirection="column" border="solid 1px #000">
+                <Text fontSize="md" as="p" align="center">
+                  Preco com desconto no boleto
+                </Text>
+                <Box alignItems="flex-start" display="flex" flex={1} flexDirection="column" border="solid 1px #000">
+                  <StatGroup justifyContent="flex-start" flexDirection="column" height={{ lg: 450, md: "auto" }}>
+                    {produto.consultas.map((consulta, index) => (
+                      <Stat borderWidth="1px" borderStyle="solid" key={consulta.id} height={150}>
+                        <StatNumber>{consulta.preco}</StatNumber>
+                        <StatHelpText>{consulta.data}</StatHelpText>
+                      </Stat>
+                    ))}
+                  </StatGroup>
+                </Box>
+              </Flex>
+            </Box>
+          </ListItem>
+        ))}
+    </List>
+  );
 }

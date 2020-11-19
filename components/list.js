6450e792d34e98c50b4d52290ca4e33a0d22955c
nelson <nelsonptobias@gmail.com>
2020-11-19 16:30:46 -0300

    [feat] adicionado tratamento de css

--- components/list.js
+++ components/list.js
@@ -1,4 +1,4 @@
-import { List, Stat, StatNumber, Text, Image, ListItem, StatHelpText, Button, Flex, Box, StatGroup, Link } from "@chakra-ui/core";
+import { List, Stat, StatNumber, Text, Image, ListItem, StatHelpText, Button, Flex, Box, StatGroup, Link, Badge, HStack } from "@chakra-ui/core";
 
 export default function ListaProdutos({ lista, pesquisar }) {
   return (
@@ -8,7 +8,9 @@ export default function ListaProdutos({ lista, pesquisar }) {
           <ListItem key={produto.id}>
             <Box display="flex" flexDirection={{ lg: "row", md: "column" }}>
               <Box display="flex" flex={1} flexDirection="column" alignItems="center" border="solid 1px #000" borderRadius="lg">
-                <Text fontSize="md">{produto.nomeProduto}</Text>
+                <Text fontWeight="semibold" fontSize="sm">
+                  {produto.nomeProduto}
+                </Text>
                 <Image src={produto.img} alt="produto" />
                 <Link href={produto.url} isExternal>
                   Link
@@ -17,16 +19,20 @@ export default function ListaProdutos({ lista, pesquisar }) {
                   Button
                 </Button>
               </Box>
-              <Flex flex={2} flexDirection="column" border="solid 1px #000" borderRadius="lg">
-                <Text fontSize="md" as="p" align="center">
+              <Flex flex={4} flexDirection="column" border="solid 1px #000" borderRadius="lg">
+                <Text fontWeight="semibold" fontSize="md" as="p" align="center">
                   Preco com desconto no boleto
                 </Text>
-                <Box bg="#ECA611" alignItems="flex-start" display="flex" flex={1} flexDirection="column">
-                  <StatGroup justifyContent="flex-start" flexDirection="column" height={{ lg: 450, md: "auto" }}>
+                <Box bg="gray.300" alignItems="flex-start" display="flex" flex={1} flexDirection="column">
+                  <StatGroup justifyContent="flex-start" flexDirection="column" height={{ lg: 380, md: "auto" }}>
                     {produto.consultas.map((consulta, index) => (
-                      <Stat bg="red.200" borderWidth="1px" borderStyle="solid" key={consulta.id} height={150} borderRadius="lg">
-                        <StatNumber>{consulta.preco}</StatNumber>
-                        <StatHelpText>{consulta.data}</StatHelpText>
+                      <Stat my="1" mx="1" align="center" bg="gray.200" borderWidth="1px" borderStyle="solid" key={consulta.id} borderRadius="lg" boxShadow="lg">
+                        <StatNumber fontSize={20} px={2} as="h2">
+                          {consulta.preco}
+                        </StatNumber>
+                        <Badge fontSize={10} variant="solid" rounded="full">
+                          {consulta.data}
+                        </Badge>
                       </Stat>
                     ))}
                   </StatGroup>
